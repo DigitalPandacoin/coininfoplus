367cc0f606e87c3915f3a0cce4f49052a8e7b341
Official <support@DigitalPandacoin.org>
2019-11-10 09:31:10 -0500

    Update coininfo.test.js
    Removed requirement for chai and added Console.log statements to display some sample Coininfo.

--- test/coininfo.test.js
+++ test/coininfo.test.js
@@ -1,8 +1,14 @@
-let chai = require('chai')
-let expect = chai.expect
-let coininfo = require('../lib/coininfo')
-describe('coininfoplus', () => {
-  it('creates 268 coins', function(){
-    expect(Object.keys(coininfo.supportedCoins).length).to.eq(268)
+//let chai = require('chai')
+//let expect = chai.expect
+let coininfo = require('coininfo')
+console.log('============', "Coininfoplus Coininfo")
+/*describe('coininfoplus', () => {
+  it('creates 273 coins', function(){
+   expect(Object.keys(coininfo.supportedCoins).length).to.eq(273)
   })
 })
+*/
+console.log('============', "Litecoin info")
+console.dir(coininfo('LTC'))
+console.log('============', "Pandacoin info")
+console.dir(coininfo('PND'))
